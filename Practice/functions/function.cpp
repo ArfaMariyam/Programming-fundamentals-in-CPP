fb904b1c9cd0b69c939b4cf438c80488a6b46bed
ArfaMariyam <arfa.mariyam.liaqat@gmail.com>
2026-06-29 01:28:39 -0700

    Reorganize practice files and add car parking project

--- Practice/functions/function.cpp
+++ Practice/functions/function.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 void subtract(int a, int b)
 {
-        cout<<"The subtract is: "<<a-(-b);
+        cout<<"The subtract is: "<<a-(-b)<<endl;
     }
     void multiplay(int a, int b)
     {
-        cout<<a*b;
+        cout<<"The multiple is: "<<a*b;
     }
 int main()
 {
     void subtract(int a, int b);
     void multiplay(int a,int b);
-    int a,b;
+    int a,b; 
     subtract(a=1,b=7);
     multiplay(a,b);
 }
