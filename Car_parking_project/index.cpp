44b69f29b40eb6ce6cfeb0e85c2ec1181d756bc0
ArfaMariyam <arfa.mariyam.liaqat@gmail.com>
2026-07-01 10:00:26 +0500

    update index.cpp

--- Car_parking_project/index.cpp
+++ Car_parking_project/index.cpp
@@ -16,9 +16,29 @@ cout<<"\t\t\t===================================================================
 cout<<endl;
 cout<<"\n\t\t\t  1. Arrival of a Bike\t\t\t  6. Departure of Bike";
 cout<<endl;
-cout<<"\n\t\t\t  1. Arrival of a Car\t\t\t  6. Departure of Car";
+cout<<"\n\t\t\t  2. Arrival of a Car\t\t\t  7. Departure of Car";
 cout<<endl;
-cout<<"\n\t\t\t  ";
+cout<<endl;
+cout<<"\n\t\t\t  3. Arrival of a Bus\t\t\t  8. Departure of Bus";
+cout<<endl;
+cout<<endl;
+cout<<"\n\t\t\t  4. Arrival of a Rishkaw\t\t\t  9. Departure of Rishkaw";
+cout<<endl;
+cout<<endl;
+cout<<"\n\t\t\t  5. Arrival of a Bicycle\t\t\t  10. Departure of Bicycle";
+cout<<endl;
+cout<<endl;
+cout<<"\t\t\t\t\t      ========================\n";
+cout<<"\t\t\t\t\t      ========================";
+cout<<endl;
+cout<<"\n\t\t\t\t\t\t11. Show the record";
+cout<<endl;
+cout<<"\n\t\t\t\t\t\t12. Edit the record";
+cout<<endl;
+cout<<"\n\t\t\t\t\t\t13. Delete the record";
+cout<<endl;
+cout<<"\n\t\t\t\t\t\t14. Exit Program";
+cout<<endl;
 }
 
 int main()
